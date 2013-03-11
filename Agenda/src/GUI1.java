24c5a3629e5cc299eb9ce5a248e97c4aad7a44ca
rvnijnatten <rvnijnatten@casema.nl>
2013-03-11 16:27:47 +0100

    klassen toegevoegd

--- Agenda/src/GUI1.java
+++ Agenda/src/GUI1.java
@@ -28,15 +28,6 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class GUI1
 {
